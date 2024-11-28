e7c4367b2a57a9be2eefb2a0f098707574fa800e
xiexiao <920759860@qq.com>
2024-11-28 23:28:43 +0800

    初始化

---
+++ solve-question-condition/src/test/java/com/jie/AppTest.java
@@ -0,0 +1,12 @@
+package com.jie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
